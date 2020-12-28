f0b129dbdcc59bbc32bed20ac4f585d195f08cbf
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-12-28 20:08:52 +0100

    tesla data import

---
+++ src/Base/Overview/RunButton.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Pars\Component\Base\Overview;
+
+
+use Pars\Component\Base\Field\Button;
+use Pars\Component\Base\Field\Icon;
+
+class RunButton extends Button
+{
+
+    /**
+     * DeleteButton constructor.
+     */
+    public function __construct(?string $path = null)
+    {
+        parent::__construct(null, null, $path);
+    }
+
+
+    protected function initialize()
+    {
+        $this->setOutline(true);
+        $this->setStyle(self::STYLE_SUCCESS);
+        $this->addInlineStyle('padding', '4px');
+        $this->addInlineStyle('line-height', '1');
+        $icon = new Icon(Icon::ICON_PLAY);
+        $icon->setWidth('16px');
+        $this->push($icon);
+        parent::initialize();
+        $this->removeOption('mr-1');
+        $this->addOption('btn-sm');
+    }
+}
