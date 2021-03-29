9c01ad0e24e32fa4844beff5c0e33c625cec85e1
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-03-30 00:08:09 +0200

    filter updates

--- src/Base/Field/Span.php
+++ src/Base/Field/Span.php
@@ -12,14 +12,31 @@ class Span extends AbstractField implements ColorAwareInterface
 {
     use ColorAwareTrait;
 
+    public function __construct(?string $content = null, ?string $label = null)
+    {
+        parent::__construct($content, $label);
+        $this->setColor(self::COLOR_DARK);
+    }
+
+
     protected function initialize()
     {
         $this->setTag('span');
-        if ($this->hasColor()) {
-            $this->addOption($this->getColor());
-        } else {
-            $this->addOption(self::COLOR_DARK);
-        }
     }
+
+    /**
+     * @param string $color
+     * @return $this|Span
+     */
+    public function setColor(string $color)
+    {
+        if ($this->hasColor()) {
+            $this->removeOption($this->getColor());
+        }
+        $this->color = $color;
+        $this->addOption($color);
+        return $this;
+    }
+
 
 }
