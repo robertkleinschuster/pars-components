ce44500399052c000039e408381c57409b69d9e4
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-11 17:10:03 +0200

    dependency refactoring

--- src/Base/Table/Tr.php
+++ src/Base/Table/Tr.php
@@ -4,12 +4,10 @@ namespace Pars\Component\Base\Table;
 
 use Pars\Bean\Type\Base\BeanAwareInterface;
 use Pars\Bean\Type\Base\BeanAwareTrait;
-use Pars\Bean\Type\Base\BeanInterface;
 use Pars\Component\Base\Field\Badge;
 use Pars\Component\Base\Field\Button;
 use Pars\Component\Base\Field\Icon;
 use Pars\Component\Base\Form\Input;
-use Pars\Core\Cache\ParsCache;
 use Pars\Mvc\View\AbstractComponent;
 use Pars\Mvc\View\FieldListAwareTrait;
 
