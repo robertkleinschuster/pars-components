007d9095b6a7a9e98c9b5cb483b5b6a54c9b830b
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-19 01:33:45 +0200

    bugs bugs bugs

--- src/Base/Filter/Filter.php
+++ src/Base/Filter/Filter.php
@@ -4,6 +4,7 @@ namespace Pars\Component\Base\Filter;
 
 use Pars\Component\Base\Edit\Edit;
 use Pars\Component\Base\Toolbar\FilterButton;
+use Pars\Mvc\View\Event\ViewEvent;
 
 /**
  * Class Filter
@@ -13,12 +14,27 @@ class Filter extends Edit
 {
     protected FilterButton $button;
 
+    protected function initEvent()
+    {
+        parent::initEvent();
+        $this->getForm()->setId($this->getId() . '_form');
+        $this->getButton()->setId($this->getId() . '_button');
+        $event = ViewEvent::createCallback(function () {
+            $this->getForm()->addOption('show');
+        });
+        $event->setPath($this->getPathHelper(false)->getPath());
+        $this->getButton()->setEvent($event);
+        $event->setTargetId($this->getForm()->getId());
+    }
+
+
     protected function initialize()
     {
         $button = $this->getButton();
+        $button->addOption('show-filter');
         $button->setData('toggle', 'collapse');
-        $button->setData('target', '#' . $this->getForm()->generateId());
-      #  $this->push($button);
+        $button->setData('target', '#' . $this->getForm()->getId());
+        #  $this->push($button);
         $this->getForm()->addOption('collapse');
         parent::initialize();
     }
