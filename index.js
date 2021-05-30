e1379894170675bf5ff50678ab227df71cec1c31
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-05-30 13:59:57 +0200

    feat(Admin): Tooltips

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './src/Base/base.scss';
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 import {Wysiwyg} from "./src/Base/Form/Wysiwyg/Wysiwyg";
 import {BulkCheckbox} from "./src/Base/Overview/BulkCheckbox";
 import {ParsButton} from "./src/Base/Field/ParsButton";
@@ -11,4 +11,13 @@ function initComponents(root, event = null) {
     Wysiwyg.init(root);
     BulkCheckbox.init(root);
     ParsButton.init(root);
+    initTooltips(root);
+}
+
+function initTooltips(root)
+{
+    let tooltipTriggerList = [].slice.call(root.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 }
